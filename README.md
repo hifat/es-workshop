3f7f928276b477d382406b7cc4a64610e2e1363c
hifat <logmanxsa@gmail.com>
2024-11-28 23:02:15 +0700

    add data set

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-YouTube: [มาลองทำ Search ด้วย Elasticsearch กัน](https://www.youtube.com/watch?v=ECnEzu9E7Wg&t=2346s&ab_channel=mikelopster)
+# Elasticsearch Workshop
+
+## Reference
+- Mikelopster: [มาลองทำ Search ด้วย Elasticsearch กัน](https://www.youtube.com/watch?v=ECnEzu9E7Wg&t=2346s&ab_channel=mikelopster)
+- CodeBangkok: [Elasticsearch: Get Started](https://www.youtube.com/watch?v=d2Ek-8HKdcI&list=PLyZTXfAT27iYpJgj--2fV7JDC7kYKYoUO&ab_channel=CodeBangkok)
+
+
+## Issue & Solve
+
+- ERROR: [1] bootstrap checks failed. You must address the points described in the following [1] lines before starting Elasticsearch. es01 | bootstrap check failure [1] of [1]: max virtual memory areas vm.max_map_count [65530] is too low ...
+
+```docker
+# Run in terminal
+docker run -it --rm --privileged --pid=host alpine nsenter -t 1 -m -u -n -i sh -c "sysctl -w vm.max_map_count=262144"
+```
--- main.go
+++ main.go
@@ -1,7 +1,70 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+
+	"github.com/elastic/go-elasticsearch/v8"
+)
+
+func ESQuery(q map[string]any) bytes.Buffer {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(q); err != nil {
+		log.Fatalf("Error encoding query: %s", err)
+	}
+
+	return buf
+}
+
+func JSONDecoder[T any](r io.Reader, result *T) {
+	if err := json.NewDecoder(r).Decode(result); err != nil {
+		log.Fatalf("Error parsing response: %s", err)
+	}
+}
 
 func main() {
-	fmt.Println("Hello, Elasticsearch!")
+	client, err := elasticsearch.NewClient(elasticsearch.Config{
+		Addresses: []string{
+			"http://localhost:9200",
+		},
+		Username: "foo",
+		Password: "bar",
+	})
+	if err != nil {
+		log.Fatalf("Error creating the client: %s", err)
+	}
+
+	query := map[string]any{
+		"size": 1,
+		"query": map[string]any{
+			"match": map[string]any{
+				"genre": "anime",
+			},
+		},
+	}
+
+	buf := ESQuery(query)
+
+	res, err := client.Search(
+		client.Search.WithContext(context.Background()),
+		client.Search.WithIndex("anime"),
+		client.Search.WithBody(&buf),
+	)
+	if err != nil {
+		log.Fatalf("Error getting response: %s", err)
+	}
+	defer res.Body.Close()
+
+	if res.IsError() {
+		log.Fatalf("Error response: %s", res.String())
+	}
+
+	var result map[string]any
+	JSONDecoder(res.Body, &result)
+
+	fmt.Printf("Found %v hits\n", result["hits"].(map[string]any)["hits"])
 }
